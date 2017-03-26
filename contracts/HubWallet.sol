d46689e0be8880e4870b26c6247b2ec43ddd58c0
JackBekket <jackatwork@yandex.ru>
2017-03-26 03:09:01 +0300

    examples

--- contracts/HubWallet.sol
+++ contracts/HubWallet.sol
@@ -3,14 +3,29 @@ pragma solidity ^0.4.4;
 //Raw prototype for Hub wallet contract.
 
 
+/* The token is used as a voting shares */
+contract token { mapping (address => uint) public balances;  }
+
 contract HubWallet{
 
 
+
+
+  ///@dev constructor
   function HubWallet(){
 
 
-    
+
   }
+
+
+  /*/
+   *  Constants
+  /*/
+
+  uint public freezePercent;
+  uint public frozenFunds;
+
 
 
 
