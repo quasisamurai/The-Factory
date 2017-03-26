08820d651e59d3d6168bd9f02313edc6c87b1a42
JackBekket <jackatwork@yandex.ru>
2017-03-26 14:36:51 +0300

    ownable

--- contracts/HubWallet.sol
+++ contracts/HubWallet.sol
@@ -16,17 +16,23 @@ contract token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
 
     function balanceOf(address _owner) constant returns (uint balance);
+    function approve(address _spender, uint _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint remaining);
+
 }
 
 
-contract HubWallet{
+contract HubWallet is Ownable{
 
 
 
 
   ///@dev constructor
-  function HubWallet(){
-
+  function HubWallet(address _hubowner,address _dao,address _whitelist){
+    owner=_hubowner;
+    DAO=_dao;
+    Whitelist=_whitelist;
+    Factory=msg.sender;
 
 
   }
@@ -36,10 +42,19 @@ contract HubWallet{
    *  Constants
   /*/
 
-  uint public freezePercent;
+  address public DAO;
+  address public Factory;
+  address public Whitelist;
+
+
+  //uint public freezePercent;
+
+  // FreezeQuote - it is defined amount of tokens need to be frozen on  this contract.
+  uint public freezeQuote;
   uint public frozenFunds;
 
-
-
+  //lockedFunds - it is lockedFunds in percentage, which will be locked for every payday period.
+  uint public lockPercent;
+  uint public lockedFunds;
 
 }
