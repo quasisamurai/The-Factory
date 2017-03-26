aa95c05595aa86e25ae955f00edbe36c606bd68c
JackBekket <jackatwork@yandex.ru>
2017-03-26 14:19:12 +0300

    token struct

--- contracts/HubWallet.sol
+++ contracts/HubWallet.sol
@@ -3,8 +3,21 @@ pragma solidity ^0.4.4;
 //Raw prototype for Hub wallet contract.
 
 
+import "zeppelin/ownership/Ownable.sol";
+
+
 /* The token is used as a voting shares */
-contract token { mapping (address => uint) public balances;  }
+
+
+
+
+contract token {
+    mapping (address => uint) public balances;
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+
+    function balanceOf(address _owner) constant returns (uint balance);
+}
+
 
 contract HubWallet{
 
