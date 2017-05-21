968b9c5dd598803cf0e80cdb3f00b998a48a659e
JackBekket <jackatwork@yandex.ru>
2017-05-21 21:17:18 +0300

    generator struct

--- contracts/generator.go
+++ contracts/generator.go
@@ -1,8 +1,8 @@
 
-//go:generate abigen --sol SDT.sol --pkg token --out SDT.go
-//go:generate abigen --sol HubWallet.sol --pkg hubwallet --out HubWallet.go
-//go:generate abigen --sol Whitelist.sol --pkg whitelist --out Whitelist.go
-//go:generate abigen --sol Factory.sol --pkg factory --out Factory.go
+//go:generate abigen --sol SDT.sol --pkg token --out ./go-build/SDT.go
+//go:generate abigen --sol HubWallet.sol --pkg hubwallet --out ./go-build/HubWallet.go
+//go:generate abigen --sol Whitelist.sol --pkg whitelist --out ./go-build/Whitelist.go
+//go:generate abigen --sol Factory.sol --pkg factory --out ./go-build/Factory.go
 package main
 
 
