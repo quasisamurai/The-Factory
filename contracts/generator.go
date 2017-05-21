1cedf8af371ed1145776bcdf12fef96093f8db74
JackBekket <jackatwork@yandex.ru>
2017-05-21 21:09:43 +0300

    generator

---
+++ contracts/generator.go
@@ -0,0 +1,12 @@
+
+//go:generate abigen --sol SDT.sol --pkg token --out SDT.go
+//go:generate abigen --sol HubWallet.sol --pkg hubwallet --out HubWallet.go
+//go:generate abigen --sol Whitelist.sol --pkg whitelist --out Whitelist.go
+//go:generate abigen --sol Factory.sol --pkg factory --out Factory.go
+package main
+
+
+
+func main()  {
+
+}
