6d8104063632e7797c4cdedc0b66f58ccd744d1c
Anton Shchukin <szwtdy@gmail.com>
2017-07-11 14:53:16 +0300

    repair migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,21 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var MetaCoin = artifacts.require("./MetaCoin.sol");
 
+var SDT = artifacts.require("./SDT.sol");
+var Factory = artifacts.require("./Factory.sol");
+var Whitelist = artifacts.require("./Whitelist.sol");
 
-
-var Token = artifacts.require("./SDT.sol");
-var Factory = artifacts.require("./Hubs&Miners/Factory.sol");
-var Whitelist = artifacts.require("./Whitelist/Whitelist.sol");
+console.log(SDT);
+console.log(Factory);
+console.log(Whitelist);
 
 module.exports = function(deployer) {
 
-  deployer.deploy(Token).then(function () {
-    return deployer.deploy(Factory, Token.address, web3.eth.accounts[0]);
-  }).then( function () {
-      return deployer.deploy(Whitelist, Factory.address);
-  });
+    deployer.deploy(SDT).then(function () {
+        return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0]);
+    })
+    .then( function () {
+        return deployer.deploy(Whitelist, Factory.address);
+    });
 
 };
