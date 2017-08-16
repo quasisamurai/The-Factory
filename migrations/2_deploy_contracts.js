601e418c13ef5a458a7bd962a303cefb5a03b0a5
quasisamurai <quazisamurai@gmail.com>
2017-08-16 16:08:25 +0300

    web3 appeared

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ var Factory = artifacts.require("./Factory.sol");
 module.exports = function(deployer) {
 
     deployer.deploy(SDT).then(function () {
-        return deployer.deploy(Factory, SDT.address,0x0 );
+        return deployer.deploy(Factory, web3.eth.accounts[0]);
     });
 
 };
