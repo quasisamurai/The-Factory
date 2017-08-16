c3553807af41f9c4e39403e86e565e1ce849bd26
quasisamurai <quazisamurai@gmail.com>
2017-08-16 16:07:12 +0300

    fixed migrations file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,11 @@
 var SDT = artifacts.require("./SDT.sol");
 var Factory = artifacts.require("./Factory.sol");
-var Whitelist = artifacts.require("./Whitelist.sol");
 
 module.exports = function(deployer) {
 
     deployer.deploy(SDT).then(function () {
-        return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0]);
-    })
-    .then( function () {
-        return deployer.deploy(Whitelist, Factory.address);
+        return deployer.deploy(Factory, SDT.address,0x0 );
     });
 
 };
+//web3.eth.accounts[0]
