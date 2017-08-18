81991c4d6bcc08fe20f66e643cbb09cfc47a0302
JackBekket <jackatwork@yandex.ru>
2017-08-18 22:48:34 +0300

    npm fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,11 @@ var Factory = artifacts.require("./Factory.sol");
 
 module.exports = function(deployer) {
 
-    deployer.deploy(SDT).then(function () {
-        return deployer.deploy(Factory,SDT.address, web3.eth.accounts[0]);
-    });
+    deployer.deploy(SDT);
+//    deployer.deploy(Factory);
+
+    deployer.deploy(Factory,SDT.address, web3.eth.accounts[0]);
+
 
 };
 //web3.eth.accounts[0]
