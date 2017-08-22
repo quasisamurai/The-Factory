0a26174ea46ef52fd79c060d3ebf9518134f884a
JackBekket <jackatwork@yandex.ru>
2017-08-22 21:57:02 +0300

    rough structs

--- contracts/Dealable.sol
+++ contracts/Dealable.sol
@@ -1,7 +1,27 @@
 pragma solidity ^0.4.11;
 
+import "./Declaration.sol";
+
 contract Dealable{
 
 
+
+  enum DealStatus {
+      None,
+      Aviable,
+      Cancelled,
+      Rejected,
+      Done
+
+  }
+
+  struct DealInfo {
+
+      address buyer;
+      uint lockedFunds;
+
+  }
+
+  mapping (uint => EscrowInfo) public deals;
 
 }
--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.4.11;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 //import "./zeppelin/ownership/Ownable.sol";
-import "./Declaration.sol";
+
 
 
 
