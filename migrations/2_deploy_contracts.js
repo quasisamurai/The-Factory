d97bf84a6872ab3ab7bd7d1a6d7f5bb57c05e328
JackBekket <jackatwork@yandex.ru>
2017-08-25 16:25:59 +0300

    test gas usage

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,16 @@
 var SDT = artifacts.require("./SDT.sol");
 var Factory = artifacts.require("./Factory.sol");
+var Profile = artifacts.require("./Profile.sol");
 
 module.exports = function(deployer) {
-
+/*
   deployer.deploy(SDT).then(function() {
   return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], SDT.address);
+
+
 });
+*/
+
+  deployer.deploy(Profile);
 
 };
