8e7a1a5a812725ad29f5fa6d90e490c4269a2c20
JackBekket <jackatwork@yandex.ru>
2017-08-25 20:13:46 +0300

    migration fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,17 @@
 var SDT = artifacts.require("./SDT.sol");
 var Factory = artifacts.require("./Factory.sol");
 var Profile = artifacts.require("./Profile.sol");
-var HP = artifacts.require("./HubProfile.sol");
+//var HP = artifacts.require("./HubProfile.sol");
+var FactoryH = artifacts.require("./FactoryH.sol");
 
 module.exports = function(deployer) {
 
   deployer.deploy(SDT).then(function() {
-  return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], SDT.address);
+    return deployer.deploy(FactoryH);
+  }).then(function () {
+
+
+  return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], FactoryH.address);
 
 
 });
