aa8fbaca5ac80cf5cb14872b582f236b78957190
JackBekket <jackatwork@yandex.ru>
2017-08-25 16:38:07 +0300

    migrations back

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,14 +3,14 @@ var Factory = artifacts.require("./Factory.sol");
 var Profile = artifacts.require("./Profile.sol");
 
 module.exports = function(deployer) {
-/*
+
   deployer.deploy(SDT).then(function() {
   return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], SDT.address);
 
 
 });
-*/
 
-  deployer.deploy(Profile);
+
+    //deployer.deploy(Profile);
 
 };
