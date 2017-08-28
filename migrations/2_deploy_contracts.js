5c4926ee92f9485bb9d7d595ffac62d58ff5cb26
quasisamurai <quazisamurai@gmail.com>
2017-08-28 15:11:55 +0300

    client factory migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,16 +4,18 @@ var Profile = artifacts.require("./Profile.sol");
 //var HP = artifacts.require("./HubProfile.sol");
 var FactoryH = artifacts.require("./FactoryH.sol");
 var FactoryM = artifacts.require("./FactoryM.sol");
+var FactoryC = artifacts.require("./FactoryC.sol");
 
 module.exports = function(deployer) {
 
   deployer.deploy(SDT).then(function() {
     return deployer.deploy(FactoryH);
   }).then(function () {
-  return deployer.deploy(FactoryM);
+    return deployer.deploy(FactoryM);
   }).then(function () {
-
-  return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], FactoryH.address, FactoryM.address);
+    return deployer.deploy(FactoryC); 
+  }).then(function () {
+      return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], FactoryH.address, FactoryM.address, FactoryC.address);
 
 
 });
