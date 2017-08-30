d9eea6607cb94f1e279349b5bb49a857194576ce
quasisamurai <quazisamurai@gmail.com>
2017-08-30 14:00:38 +0300

    deployin in acc. w truffle specs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,9 +16,9 @@ module.exports = function(deployer) {
   }).then(function () {
     return deployer.deploy(FactoryC);
   }).then(function () {
-      return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], FactoryH.address, FactoryM.address, FactoryC.address);
+      return deployer.deploy(Factory, SDT.address, web3.eth.accounts[0], FactoryH.address, FactoryM.address, FactoryC.address, SDT.address, FactoryH.address, FactoryM.address, FactoryC.address);
   }).then(function () {
-      return deployer.deploy(Network,Factory.address);
+      return deployer.deploy(Network,Factory.address, SDT.address, FactoryH.address, FactoryM.address, FactoryC.address, Factory.address);
 });
 
 
