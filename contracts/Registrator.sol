669f47d7831a18240463c1f8e107e16c70b3ce65
JackBekket <jackatwork@yandex.ru>
2017-10-03 23:25:38 +0300

    skeleton

--- contracts/Registrator.sol
+++ contracts/Registrator.sol
@@ -13,6 +13,7 @@ pragma solidity ^0.4.11;
 
 contract Registrator {
 
+
   /*
   // tyoe of entangle link
   enum TypeL {
@@ -28,16 +29,38 @@ contract Registrator {
 
 
   struct SocLinks {
-    bytes64 fcb;
-    bytes64 vk;
-    bytes64 git;
-    bytes64 eml;
-    bytes64 tlgr;
+    bytes32 fcb;
+    bytes32 vk;
+    bytes32 git;
+    bytes32 eml;
+    bytes32 tlgr;
   }
 
-
+//  TypeL public linktype
 
   mapping (address => SocLinks) public entangled;
+
+  /* NOTE I should notice, that we cannot simply use array of links cause every link is bytes32 array itself.
+
+    Also I should NOTE, that this contract should be deployed from us (and not from user) + it should be cheap in use for user, so
+    I think we need to write it in style with many different fuctions. So we can avoid unaccecary chek.
+
+  */
+
+
+  function facebook (bytes32 link) public returns (bool success) {
+
+    //Appendix for authorization
+
+    //Appendix for Profile address getter
+
+    address profile = msg.sender;
+
+    SocLinks links;
+    links.fcb = link;
+
+
+  }
 
 
 
